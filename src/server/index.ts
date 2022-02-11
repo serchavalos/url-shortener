4e96185a31857a56a1123b48526f4e921b13522c
Sergio Avalos <sergio.avalos@gmail.com>
2022-02-11 11:40:45 +0100

    Remove execution line used for testing

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,8 @@
 import { app } from "./app";
-import { addNewAlias } from "./db";
 
 const port = process.env.PORT;
 const domain = process.env.DOMAIN;
 
 app.listen(port, async () => {
-  await addNewAlias("fb", "https://www.facebook.com");
   return console.log(`Server is listening at http://${domain}:${port}`);
 });
